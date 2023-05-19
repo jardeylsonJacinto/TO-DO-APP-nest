bc899983a6cf9985bf8c897ecf06ada3eb13994e
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-05-19 11:40:50 -0300

    feat: Create the PUT route function

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put } from '@nestjs/common';
 import { TodoDTO } from './todo.dto';
 import { todos } from './todos-mock';
 
@@ -21,5 +21,11 @@ export class TodoController {
     todosData = [...todosData, newTodo];
 
     return newTodo;
+  }
+
+  @Put(':id')
+  updateTodo(@Body() updateTodo: TodoDTO, @Param(':id') id): TodoDTO {
+    todosData = todosData.map((todo) => (todo.id === id ? updateTodo : todo));
+    return updateTodo;
   }
 }
