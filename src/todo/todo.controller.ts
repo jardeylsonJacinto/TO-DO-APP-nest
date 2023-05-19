ee85a12a8361d781ed3530eb8e86f90f8dea2da7
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-05-19 11:24:36 -0300

    feat: Create the POST route function

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TodoDTO } from './todo.dto';
 import { todos } from './todos-mock';
 
-const todosData = todos;
+let todosData = todos;
 
 @Controller('todos')
 export class TodoController {
   @Get()
   getTodos(): TodoDTO[] {
     return todosData;
+  }
+
+  @Post()
+  createTodo(@Body() createTodo: TodoDTO): TodoDTO {
+    const newTodo: TodoDTO = {
+      id: (todosData.length + 1).toString(),
+      ...createTodo,
+    };
+
+    todosData = [...todosData, newTodo];
+
+    return newTodo;
   }
 }
