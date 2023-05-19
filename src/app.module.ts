60d9df755ac1d9f0524028c077cfb9103affb665
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-05-19 10:20:13 -0300

    feat: created the mocks

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { MongooseModule } from '@nestjs/mongoose';
+import { TodoController } from './todo/todo/todo.controller';
 
 @Module({
   imports: [MongooseModule.forRoot('')],
-  controllers: [AppController],
+  controllers: [AppController, TodoController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ todos-mock.ts
@@ -0,0 +1,24 @@
+import { TodoDTO } from './src/todo/toto.dto';
+
+export const todos: TodoDTO[] = [
+  {
+    id: '1',
+    title: 'generate apps',
+    status: 'done',
+  },
+  {
+    id: '2',
+    title: 'Set up the backend',
+    status: 'in progress',
+  },
+  {
+    id: '3',
+    title: 'Coneect BD',
+    status: 'todo',
+  },
+  {
+    id: '4',
+    title: 'Set up the client',
+    status: 'todo',
+  },
+];
